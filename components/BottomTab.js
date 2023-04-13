cc086f5f0a84a43a9d167bf0b46eead4cfa64c57
Natnael Gebremichael <natnael92460@gmail.com>
2023-04-13 16:35:13 +1200

    moving repo. updating with latest changes

--- components/BottomTab.js
+++ components/BottomTab.js
@@ -1,17 +1,18 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Entypo from "react-native-vector-icons/Entypo";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const BottomTab = (isOnline) => {
   return (
     <View style={styles.bottomContainer}>
-      <Entypo name={"menu"} size={24} color={"#4a4a4a"} />
+      <Ionicons name={"options"} size={30} color={"#4a4a4a"} />
       {isOnline.isOnline ? (
         <Text style={styles.bottomText}> You are Online</Text>
       ) : (
         <Text style={styles.bottomText}> You are Offline</Text>
       )}
-      <Entypo name={"menu"} size={24} color={"#4a4a4a"} />
+      <Ionicons name={"options"} size={30} color={"#4a4a4a"} />
     </View>
   );
 };
